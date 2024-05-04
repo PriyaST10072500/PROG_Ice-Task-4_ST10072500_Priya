7b37fe8badebf0e6b3cab069481c3eaf6274af67
Priya <priyaboodhoo365@gmail.com>
2024-05-04 13:34:31 +0200

    Unit Tests
    Pushing to GitHub the Unit Tests for Ice Task 4

---
+++ UnitTests.cs
@@ -0,0 +1,20 @@
+using PROG_Ice_Task_4_ST10072500_Priya;
+
+namespace Smart_Home_Event_Handling_System_UnitTests
+{
+    [TestClass]
+    public class UnitTests
+        {
+        private EventHub eventHub;
+
+
+        //Test Method for SetUp
+        [TestMethod]
+        public void Setup()
+        {
+            eventHub = new EventHub();
+        }
+        
+
+    }
+}
