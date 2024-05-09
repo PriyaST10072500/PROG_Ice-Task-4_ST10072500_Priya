602d2fb2173d48c419ec46b0ca9f3dcc5be237ae
Priya <priyaboodhoo365@gmail.com>
2024-05-09 20:46:05 +0200

    Update UnitTests.cs
    Updated Unit Tests

--- UnitTests.cs
+++ UnitTests.cs
@@ -14,7 +14,28 @@ namespace Smart_Home_Event_Handling_System_UnitTests
         {
             eventHub = new EventHub();
         }
-        
 
+
+        //Test Method for Thermostat
+        [TestMethod]
+        public void TestMethod2()
+        {
+            int thermostatId = 1;
+            double initialTemperature = 20.0;
+            double newTemperature = 25.0;
+
+            var eventHub = new EventHub();
+
+            var thermostat = new Thermostat(thermostatId, initialTemperature);
+
+            eventHub.TemperatureChanged += (sender, args) =>
+            {
+                Assert.AreEqual(thermostatId, args.ThermostatId);
+                Assert.AreEqual(newTemperature, args.NewTemperature);
+            };
+            
+            thermostat.UpdateTemperature(eventHub, newTemperature);
+        }
+        
     }
 }
